3fab1570ca1eb98ea89ca97fce19939014ae6c4e
kevin rodriguez <kevin.rodriguez@dodifferent.it>
2021-04-19 12:50:51 +0200

    Added screens files: home, login and social login with their routes in the main file. Added Api class for backend calls

---
+++ subscription_app/lib/services/api.dart
@@ -0,0 +1,57 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Api {
+  Api({this.path, this.token});
+
+  final String path;
+  final String token;
+  final String _baseUrl = "127.0.0.1:8000";
+
+  Future<dynamic> get() async {
+    try {
+      final response = await http.get(
+        Uri.http(_baseUrl, path),
+        headers: <String, String>{
+          'Accept': 'application/json;',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        String data = response.body;
+
+        return data;
+      } else {
+        print(response.statusCode);
+        return null;
+      }
+    } catch (e) {
+      print('Error $e');
+      return null;
+    }
+  }
+
+  Future<dynamic> post(dynamic body) async {
+    try {
+      final response = await http.post(Uri.http(_baseUrl, path),
+          headers: <String, String>{
+            'Accept': 'application/json',
+            'Authorization': 'Bearer $token'
+          },
+          body: jsonEncode(body));
+
+      if (response.statusCode == 200) {
+        String data = response.body;
+
+        return data;
+      } else {
+        print(response.statusCode);
+        return null;
+      }
+    } catch (e) {
+      print('Error $e');
+      return null;
+    }
+  }
+}
