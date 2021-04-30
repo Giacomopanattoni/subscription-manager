941f3eec1fa7cf6804fe8201c888ae0feca7d360
Giacomo Panattoni <giacomo.panattoni@dodifferent.it>
2021-04-30 14:26:02 +0200

    remove websocket

--- subscription_app/lib/services/notifications.dart
+++ subscription_app/lib/services/notifications.dart
@@ -1,30 +1,7 @@
-import 'package:laravel_echo/laravel_echo.dart';
-import 'package:pusher_client/pusher_client.dart';
+
 
 class Notifications {
-  Echo echo;
 
-  Notifications() {
-    initPusher();
-  }
+  Notifications()
 
-  void initPusher() {
-    PusherOptions options = PusherOptions(
-      host: 'subscriptionmanager.foolstack.app',
-      wssPort: 6001,
-      encrypted: true,
-    );
-    PusherClient pusher =
-        PusherClient('subscriptionmanager', options, autoConnect: true);
-
-    echo = new Echo({
-      'broadcaster': 'pusher',
-      'client': pusher,
-    });
-
-    echo.channel('user').listen('.user.channel', (PusherEvent e) {
-      print(e.data);
-      //TODO implementare notifiche
-    });
-  }
 }
