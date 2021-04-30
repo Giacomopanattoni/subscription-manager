859f0550d1ee7fbbdf8afdb81f7bd78f6938cdc9
kevin rodriguez <kevin.rodriguez@dodifferent.it>
2021-04-30 11:33:25 +0200

    Removed socialLogin_screen Renamed login_screen in auth_screen Fix services auth with social

---
+++ subscription_app/lib/constants/auth.dart
@@ -0,0 +1,5 @@
+const kClientSecret = 'bVQwHIHugs8ZJl8PWVmd2taJ8297lQBW6ODrJIoF';
+const kClientId = '3';
+const kGrantTypePassword = 'password';
+const kGrantTypeSocial = 'social';
+const kProviderGoogle = 'google';
--- subscription_app/lib/main.dart
+++ subscription_app/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:subscription_app/screens/socialLogin_screen.dart';
 import 'package:subscription_app/services/appSettings.dart';
-import 'screens/login_screen.dart';
+import 'screens/auth_screen.dart';
 import 'screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -13,18 +13,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => AppSettings(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<AppSettings>(create: (context) => AppSettings()),
+      ],
       child: MaterialApp(
         title: 'Flutter Demo',
         initialRoute: '/',
         routes: {
-          SocialLoginScreen.id: (context) => SocialLoginScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
+          AuthScreen.id: (context) => AuthScreen(),
           HomeScreen.id: (context) => HomeScreen(),
         },
         theme: ThemeData(),
-        home: HomeScreen(),
+        home:AuthScreen(),
       ),
     );
   }
